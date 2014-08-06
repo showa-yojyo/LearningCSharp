d089a188f0bc9e388ec0e61df661d63a066acca3
showa_yojyo <yojyo@hotmail.com>
2014-08-07 01:23:34 +0900

    (LinqDemoCast.cs) add tests.

--- src/TestLinq/LinqDemoCast.cs
+++ src/TestLinq/LinqDemoCast.cs
@@ -9,12 +9,31 @@ namespace LinqDemo
     public class LinqDemoCast
     {
         /// <summary>
-        /// 
+        /// OfType êÑèßÅB
         /// </summary>
         [TestMethod]
         public void TestCast()
         {
-            Assert.Fail();
+            var source = new List<object>
+            {
+                0.0, 0.1, 0.2, 0.3, 0.4,
+            };
+
+            var result = source.Cast<double>()
+                .Zip(source, (first, second) => Math.Abs(first - (double)second));
+            Assert.IsTrue(result.All(i => i < 1e-8));
+        }
+
+        /// <summary>
+        /// OfType êÑèßÅB
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void TestCastException()
+        {
+            var source1 = Enumerable.Range(0, 10).Cast<double>();
+
+            source1.First();
         }
     }
 }
