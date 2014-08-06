4c96c094784b01a5e086faf89c5c5c161d9406ec
showa_yojyo <yojyo@hotmail.com>
2014-08-07 01:56:10 +0900

    (LinqDemoSelect.cs) add tests.

--- src/TestLinq/LinqDemoSelect.cs
+++ src/TestLinq/LinqDemoSelect.cs
@@ -9,12 +9,32 @@ namespace LinqDemo
     public class LinqDemoSelect
     {
         /// <summary>
-        /// 
+        /// 基本中の基本。
         /// </summary>
         [TestMethod]
         public void TestSelect()
         {
-            Assert.Fail();
+            var source = "ABC";
+            var result = source.Select(c => char.ToLower(c));
+
+            Assert.AreEqual(result.ElementAt(0), 'a');
+            Assert.AreEqual(result.ElementAt(1), 'b');
+            Assert.AreEqual(result.ElementAt(2), 'c');
+        }
+
+        /// <summary>
+        /// Select も添字版がある。
+        /// </summary>
+        [TestMethod]
+        public void TestSelectIndexed()
+        {
+            var source = "ABC";
+            var result = source.Select(
+                (c, i) => new string(c, i));
+
+            Assert.AreEqual(result.ElementAt(0), "");
+            Assert.AreEqual(result.ElementAt(1), "B");
+            Assert.AreEqual(result.ElementAt(2), "CC");
         }
     }
 }
