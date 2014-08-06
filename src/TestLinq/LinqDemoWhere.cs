71b26b666dc4fcf026a3b7b3ee7823d5575ecf6f
showa_yojyo <yojyo@hotmail.com>
2014-08-07 01:04:34 +0900

    (LinqDemoWhere.cs) add tests.

--- src/TestLinq/LinqDemoWhere.cs
+++ src/TestLinq/LinqDemoWhere.cs
@@ -5,16 +5,36 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LinqDemo
 {
+    /// <summary>
+    /// Where をフィルターとして利用する。
+    /// </summary>
     [TestClass]
     public class LinqDemoWhere
     {
         /// <summary>
-        /// 
+        /// 基本中の基本。
         /// </summary>
         [TestMethod]
         public void TestWhere()
         {
-            Assert.Fail();
+            var source = Enumerable.Range(0, 100);
+            var even = source.Where(i => i % 2 == 0);
+
+            Assert.IsTrue(even.All(i => i % 2 == 0));
+        }
+
+        /// <summary>
+        /// 添字付きバージョン。
+        /// </summary>
+        [TestMethod]
+        public void TestWhereIndexed()
+        {
+            var source = Enumerable.Repeat(50, 100);
+            var result = source.Where(
+                (val, i) => i < val);
+
+            Assert.AreEqual(result.Count(), 50); // [50, 100)
+            Assert.IsTrue(result.All(i => i == 50));
         }
     }
 }
