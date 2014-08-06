da364aeb42f63a55187c6d00f0bb15a82b38acae
showa_yojyo <yojyo@hotmail.com>
2014-08-07 01:41:04 +0900

    (LinqDemoLongCount.cs) add tests.

--- src/TestLinq/LinqDemoLongCount.cs
+++ src/TestLinq/LinqDemoLongCount.cs
@@ -9,12 +9,25 @@ namespace LinqDemo
     public class LinqDemoLongCount
     {
         /// <summary>
-        /// 
+        /// LongCount は Count の大容量コレクション版？
+        /// </summary>
+        //[TestMethod]
+        //public void TestLongCountDangerous()
+        //{
+        //    var source = Enumerable.Repeat(0, Int32.MaxValue);
+        //    var result = source.Concat(source);
+
+        //    Assert.AreEqual(result.LongCount(), (long)Int32.MaxValue * 2);
+        //}
+
+        /// <summary>
+        /// LongCount は Count の大容量コレクション版？
         /// </summary>
         [TestMethod]
         public void TestLongCount()
         {
-            Assert.Fail();
+            var source = Enumerable.Repeat(0, 3);
+            Assert.AreEqual(source.LongCount(), 3L);
         }
     }
 }
