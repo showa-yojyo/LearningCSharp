f559a43799576fab946d2220b2526c39f28f7a45
showa_yojyo <yojyo@hotmail.com>
2014-08-07 01:27:13 +0900

    (LinqDemoOfType.cs) add tests.

--- src/TestLinq/LinqDemoOfType.cs
+++ src/TestLinq/LinqDemoOfType.cs
@@ -9,12 +9,31 @@ namespace LinqDemo
     public class LinqDemoOfType
     {
         /// <summary>
-        /// 
+        /// OfType êÑèßÅB
         /// </summary>
         [TestMethod]
         public void TestOfType()
         {
-            Assert.Fail();
+            var source = new List<object>
+            {
+                0.0, 0.1, 0.2, 0.3, 0.4,
+                "a", "b", "c",
+            };
+
+            var result = source.OfType<double>()
+                .Zip(source, (first, second) => Math.Abs(first - (double)second));
+            Assert.IsTrue(result.All(i => i < 1e-8));
+        }
+
+        /// <summary>
+        /// OfType êÑèßÅB
+        /// </summary>
+        [TestMethod]
+        public void TestOfTypeSafe()
+        {
+            var source = Enumerable.Range(0, 10).OfType<double>();
+
+            Assert.IsFalse(source.Any());
         }
     }
 }
