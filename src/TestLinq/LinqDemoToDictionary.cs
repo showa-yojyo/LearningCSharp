a55f9d57d6732eaa3072f9566c7440bfc1f7db64
showa_yojyo <yojyo@hotmail.com>
2014-08-09 01:13:44 +0900

    (LinqDemoToDictionary.cs) add tests.

--- src/TestLinq/LinqDemoToDictionary.cs
+++ src/TestLinq/LinqDemoToDictionary.cs
@@ -9,12 +9,68 @@ namespace LinqDemo
     public class LinqDemoToDictionary
     {
         /// <summary>
-        /// 
+        /// ToDictionary は少々くせがある。
         /// </summary>
         [TestMethod]
         public void TestToDictionary()
         {
-            Assert.Fail();
+            var source = new[]
+            {
+                new { Id=570, Name="apple",},
+                new { Id=773, Name="banana",},
+            };
+
+            var result = source.ToDictionary(i => i.Id);
+
+            Assert.AreEqual(result[570].Name, "apple");
+            Assert.AreEqual(result[773].Name, "banana");
+        }
+
+        private class Dummy
+        {
+            public int Id
+            {
+                get;
+                set;
+            }
+
+            public string Name
+            {
+                get;
+                set;
+            }
+        };
+
+        /// <summary>
+        /// 空の列を ToDictionary しても問題ない。
+        /// </summary>
+        [TestMethod]
+        public void TestToDictionaryEmptySeq()
+        {
+            var source = Enumerable.Repeat(new Dummy
+            {
+                Id = 0,
+                Name = "",
+            }, 0);
+            var result = source.ToDictionary(i => i.Id);
+
+            Assert.IsFalse(result.Any());
+        }
+
+        /// <summary>
+        /// キー重複を引き起こすのは例外。
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestToDictionaryArgumentException()
+        {
+            var source = Enumerable.Repeat(new Dummy
+            {
+                Id = 0,
+                Name = "",
+            }, 10);
+            
+            source.ToDictionary(i => i.Id);
         }
     }
 }
