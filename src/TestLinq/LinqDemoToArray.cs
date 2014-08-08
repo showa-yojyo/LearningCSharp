923942c022389ddb26aed96e44197186dedc8a2c
showa_yojyo <yojyo@hotmail.com>
2014-08-09 00:56:16 +0900

    (LinqDemoToArray.cs, LinqDemoToList.cs) add tests.

--- src/TestLinq/LinqDemoToArray.cs
+++ src/TestLinq/LinqDemoToArray.cs
@@ -9,12 +9,27 @@ namespace LinqDemo
     public class LinqDemoToArray
     {
         /// <summary>
-        /// 
+        /// 単に配列にコンバートするだけ。
         /// </summary>
         [TestMethod]
         public void TestToArray()
         {
-            Assert.Fail();
+            var source = Enumerable.Range(0, 10);
+            var result = source.ToArray();
+
+            Assert.IsTrue(source.SequenceEqual(result));
+        }
+
+        /// <summary>
+        /// 空の範囲を ToArray すると、長さゼロの配列を得られる。
+        /// </summary>
+        [TestMethod]
+        public void TestToArrayEmptySeq()
+        {
+            var source = Enumerable.Repeat(0, 0);
+            var result = source.ToArray();
+
+            Assert.AreEqual(result.Length, 0);
         }
     }
 }
--- src/TestLinq/LinqDemoToList.cs
+++ src/TestLinq/LinqDemoToList.cs
@@ -9,12 +9,27 @@ namespace LinqDemo
     public class LinqDemoToList
     {
         /// <summary>
-        /// 
+        /// 単に List にコンバートするだけ。
         /// </summary>
         [TestMethod]
         public void TestToList()
         {
-            Assert.Fail();
+            var source = Enumerable.Range(0, 10);
+            var result = source.ToList();
+
+            Assert.IsTrue(source.SequenceEqual(result));
+        }
+
+        /// <summary>
+        /// 空の範囲を ToList すると、長さゼロの List を得られる。
+        /// </summary>
+        [TestMethod]
+        public void TestToListEmptySeq()
+        {
+            var source = Enumerable.Repeat(0, 0);
+            var result = source.ToList();
+
+            Assert.AreEqual(result.Count, 0);
         }
     }
 }
